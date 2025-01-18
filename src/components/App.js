e2f19e52238566493d2b26086bdce6fa4dc26dd0
Louis <cheruiyotlouis@gmail.com>
2025-01-18 23:56:29 +0300

    Firts commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "./components/Header";
+import About from "./components/About";
+import ArticleList from "./components/ArticleList";
+import Article from "./components/Article";
 
 console.log(blogData);
 
@@ -8,6 +12,10 @@ function App() {
     <div className="App">
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
+      <Header/>
+      <About/>
+      <ArticleList/>
+      <Article/>
     </div>
   );
 }
